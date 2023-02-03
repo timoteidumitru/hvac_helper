6b0462daa31f84f68070a546452ccb12bcb2bc21
Timotei Dumitru <dumitru.timy@gmail.com>
2023-02-03 22:39:32 +0000

    adding routing system using react-router-dom

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,25 @@
 import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Login from './components/login/Login';
+import Register from './components/register/Register';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Login />
+  },
+  {
+    path: '/register',
+    element: <Register />
+  },
+  {
+    path: '*',
+    element: <Login />
+  }
+]);
 
 function App() {
-  return <div>HI</div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
