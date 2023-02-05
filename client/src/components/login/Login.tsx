c4c7662c79b86c3309a65368667833818e57464f
Timotei Dumitru <dumitru.timy@gmail.com>
2023-02-05 20:37:27 +0000

    adding login/register UI screens

--- client/src/components/login/Login.tsx
+++ client/src/components/login/Login.tsx
@@ -1,3 +1,30 @@
+import { Link } from 'react-router-dom';
+import './login.scss';
+
 export default function Login() {
-  return <div>Login</div>;
+  return (
+    <div className="login-wrapper">
+      <div className="login-main">
+        <div className="login-top">
+          <h2>Login</h2>
+        </div>
+        <div className="login-bottom">
+          <div className="login-email">
+            <label htmlFor="email">Email: </label>
+            <input type="email" name="email" autoComplete="off" />
+          </div>
+          <div className="login-password">
+            <label htmlFor="password">Password: </label>
+            <input type="password" name="password" autoComplete="off" />
+          </div>
+          <button type="submit" className="login-botton">
+            Login
+          </button>
+          <p className="login-redirect">
+            Don't have an account? <Link to="/register">Register</Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
--- client/src/components/register/Register.tsx
+++ client/src/components/register/Register.tsx
@@ -1,3 +1,34 @@
+import './register.scss';
+import { Link } from 'react-router-dom';
+
 export default function Register() {
-  return <div>Register</div>;
+  return (
+    <div className="register-wrapper">
+      <div className="register-main">
+        <div className="register-top">
+          <h2>Register</h2>
+        </div>
+        <div className="register-bottom">
+          <div className="register-email">
+            <label htmlFor="email">Email: </label>
+            <input type="email" name="email" autoComplete="off" />
+          </div>
+          <div className="register-password">
+            <label htmlFor="password">Password: </label>
+            <input type="password" name="password" autoComplete="off" />
+          </div>
+          <div className="register-confirm-password">
+            <label htmlFor="confirm-password">Repeat Password: </label>
+            <input type="password" name="confirm-password" autoComplete="off" />
+          </div>
+          <button type="submit" className="register-botton">
+            Register
+          </button>
+          <p className="register-redirect">
+            Already have an account? <Link to="/">Login</Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
