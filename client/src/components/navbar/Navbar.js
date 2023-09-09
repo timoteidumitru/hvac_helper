30bf29d15656d6b111e6f8b300774c926dfefb13
Timotei Dumitru <dumitru.timy@gmail.com>
2023-09-09 22:12:12 +0100

    feat: menu on dashboard

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -1,39 +1,153 @@
-import React from "react";
-import { AppBar, Toolbar, Typography, Button } from "@mui/material";
-import { useAuth } from "../../context/AuthContext"; // Import the useAuth hook
-import { useNavigate } from "react-router-dom";
+import React, { useState } from "react";
+import {
+  Drawer,
+  List,
+  ListItem,
+  ListItemText,
+  ListItemIcon,
+  Button,
+  AppBar,
+  Toolbar,
+  IconButton,
+  Avatar,
+  Menu,
+  MenuItem,
+  Typography, // Add Typography
+} from "@mui/material";
+import { useAuth } from "../../context/AuthContext";
+import { useNavigate, useLocation } from "react-router-dom";
+import {
+  Home as HomeIcon,
+  Work as WorkIcon,
+  CalendarToday as CalendarIcon,
+  Settings as SettingsIcon,
+  ChevronLeft as ChevronLeftIcon,
+  ChevronRight as ChevronRightIcon,
+} from "@mui/icons-material";
 
 const NavBar = () => {
-  const { user, logout } = useAuth(); // Access the user and logout function from the context
+  const { user, logout } = useAuth();
   const navigate = useNavigate();
+  const location = useLocation(); // Get the current location
 
-  const handleLogout = () => {
-    logout(); // Call the logout function from the context
-    navigate("/login"); // Redirect to the login page after logging out
+  const [drawerOpen, setDrawerOpen] = useState(false);
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleToggleDrawer = () => {
+    setDrawerOpen(!drawerOpen);
   };
 
+  const handleLogout = () => {
+    logout();
+    navigate("/login");
+  };
+
+  const handleAvatarClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
+
+  const menuItems = [
+    {
+      label: "Home",
+      icon: <HomeIcon />,
+    },
+    {
+      label: "Work",
+      icon: <WorkIcon />,
+    },
+    {
+      label: "Calendar",
+      icon: <CalendarIcon />,
+    },
+    {
+      label: "Settings",
+      icon: <SettingsIcon />,
+    },
+  ];
+
   return (
-    <AppBar position="static">
-      <Toolbar>
-        <Typography variant="h6" style={{ flexGrow: 1 }}>
-          Your App
-        </Typography>
-        {user ? (
+    <>
+      <AppBar position="static">
+        <Toolbar style={{ justifyContent: "space-between" }}>
           <div>
-            <Typography variant="body1" style={{ marginRight: "16px" }}>
-              Welcome, {user.fullName.split(" ")[0]}!
-            </Typography>
-            <Button color="inherit" onClick={handleLogout}>
-              Logout
-            </Button>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              onClick={handleToggleDrawer}
+              edge="start"
+              style={{ fontSize: "32px", fontWeight: "700" }}
+            >
+              {drawerOpen ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+            </IconButton>
           </div>
-        ) : (
-          <Button color="inherit" onClick={() => navigate("/login")}>
-            Login
-          </Button>
-        )}
-      </Toolbar>
-    </AppBar>
+          {/* Display the current route */}
+          <Typography variant="h6" align="center" color="white">
+            {location.pathname.split("/")[1].toUpperCase()}
+          </Typography>
+          {user ? (
+            <div>
+              <IconButton
+                color="inherit"
+                aria-label="user menu"
+                aria-controls="user-menu"
+                aria-haspopup="true"
+                onClick={handleAvatarClick}
+                style={{ width: "42px", height: "42px" }}
+              >
+                <Avatar
+                  src={user.avatarUrl}
+                  alt={user.fullName}
+                  style={{ width: "100%", height: "100%" }}
+                />
+              </IconButton>
+              <Menu
+                id="user-menu"
+                anchorEl={anchorEl}
+                open={Boolean(anchorEl)}
+                onClose={handleMenuClose}
+              >
+                <MenuItem onClick={handleMenuClose}>My Account</MenuItem>
+                <MenuItem onClick={handleMenuClose}>Messages</MenuItem>
+                <MenuItem onClick={handleLogout}>Logout</MenuItem>
+              </Menu>
+            </div>
+          ) : (
+            <Button color="inherit" onClick={() => navigate("/login")}>
+              Login
+            </Button>
+          )}
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        anchor="left"
+        open={drawerOpen}
+        onClose={handleToggleDrawer}
+        variant="temporary"
+        ModalProps={{
+          keepMounted: true,
+        }}
+      >
+        <List>
+          {menuItems.map((item, index) => (
+            <ListItem
+              button
+              key={index}
+              onClick={() => {
+                handleToggleDrawer();
+                navigate(item.route);
+              }}
+            >
+              <ListItemIcon>{item.icon}</ListItemIcon>
+              <ListItemText primary={item.label} />
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+    </>
   );
 };
 
--- client/src/pages/dashboard/Dashboard.js
+++ client/src/pages/dashboard/Dashboard.js
@@ -1,58 +1,106 @@
 import React, { useState } from "react";
-import { Typography, Box, Paper, Tabs, Tab } from "@mui/material";
+import {
+  Typography,
+  Paper,
+  Tabs,
+  Tab,
+  Box,
+  Drawer,
+  List,
+  ListItem,
+  ListItemText,
+  ListItemIcon,
+} from "@mui/material";
 import { useAuth } from "../../context/AuthContext"; // Import the useAuth hook
 import NavBar from "../../components/navbar/Navbar";
+import {
+  Home as HomeIcon,
+  Work as WorkIcon,
+  CalendarToday as CalendarIcon,
+  Settings as SettingsIcon,
+} from "@mui/icons-material"; // Import icons from Material-UI
 
 const Dashboard = () => {
   const classes = useStyles();
   const { user } = useAuth(); // Access the user data and logout function from the context
   const [activeTab, setActiveTab] = useState(0);
+  const [drawerOpen, setDrawerOpen] = useState(false);
 
   const handleTabChange = (event, newValue) => {
     setActiveTab(newValue);
   };
 
+  const toggleDrawer = () => {
+    setDrawerOpen(!drawerOpen);
+  };
+
+  const menuItems = [
+    { label: "Home", icon: <HomeIcon /> },
+    { label: "Work", icon: <WorkIcon /> },
+    { label: "Calendar", icon: <CalendarIcon /> },
+    { label: "Settings", icon: <SettingsIcon /> },
+  ];
+
   return (
     <div className={classes.dashboardContainer}>
-      <NavBar />
-      <Typography variant="h4" align="center">
-        Welcome to the Dashboard
-      </Typography>
-
-      {user && (
-        <div>
-          <Paper square>
-            <Tabs
-              value={activeTab}
-              indicatorColor="primary"
-              textColor="primary"
-              onChange={handleTabChange}
-              style={classes.centeredTabs} // Apply the custom style to center the tabs
-            >
-              <Tab label="Today" />
-              <Tab label="Weekly" />
-              <Tab label="Monthly" />
-            </Tabs>
-          </Paper>
-          <Box mt={2}>
-            {activeTab === 0 && (
-              <Typography variant="body1" paragraph>
-                Content for Today
-              </Typography>
-            )}
-            {activeTab === 1 && (
-              <Typography variant="body1" paragraph>
-                Content for Weekly
-              </Typography>
-            )}
-            {activeTab === 2 && (
-              <Typography variant="body1" paragraph>
-                Content for Monthly
-              </Typography>
-            )}
-          </Box>
-        </div>
-      )}
+      <NavBar toggleDrawer={toggleDrawer} />
+      <Drawer
+        anchor="left"
+        open={drawerOpen}
+        onClose={toggleDrawer}
+        variant="temporary"
+        ModalProps={{
+          keepMounted: true,
+        }}
+      >
+        <List>
+          {menuItems.map((item, index) => (
+            <ListItem button key={index}>
+              <ListItemIcon>{item.icon}</ListItemIcon>
+              <ListItemText primary={item.label} />
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+      <main className={classes.content}>
+        <Typography variant="h4" align="center">
+          Welcome to the Dashboard
+        </Typography>
+        {user && (
+          <div>
+            <Paper square>
+              <Tabs
+                value={activeTab}
+                indicatorColor="primary"
+                textColor="primary"
+                onChange={handleTabChange}
+                style={classes.centeredTabs}
+              >
+                <Tab label="Today" />
+                <Tab label="Weekly" />
+                <Tab label="Monthly" />
+              </Tabs>
+            </Paper>
+            <Box mt={2}>
+              {activeTab === 0 && (
+                <Typography variant="body1" paragraph>
+                  Content for Today
+                </Typography>
+              )}
+              {activeTab === 1 && (
+                <Typography variant="body1" paragraph>
+                  Content for Weekly
+                </Typography>
+              )}
+              {activeTab === 2 && (
+                <Typography variant="body1" paragraph>
+                  Content for Monthly
+                </Typography>
+              )}
+            </Box>
+          </div>
+        )}
+      </main>
     </div>
   );
 };
@@ -61,11 +109,14 @@ export default Dashboard;
 
 const useStyles = () => ({
   dashboardContainer: {
-    margin: 0, // Remove the default margin
-    padding: 0, // Optionally, remove padding if needed
+    display: "flex",
+  },
+  content: {
+    flexGrow: 1,
+    padding: "20px",
   },
   centeredTabs: {
     display: "flex",
-    justifyContent: "space-around", // Update to "space-around" to evenly space the tabs
+    justifyContent: "space-around",
   },
 });
